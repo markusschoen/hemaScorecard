82c210ba213242f75a749e604c54f65ef34be45b
Markus Schön <markus.schoen@uni-ulm.de>
2025-03-31 11:16:52 +0200

    [REMOVE] TiedFighter Warning since it leads to SQL error

--- includes/header.php
+++ includes/header.php
@@ -657,34 +657,34 @@ function displayEventAnnouncements(){
 	$currentTime 	= time();
 
 
-// - Add a warning if there are ties in the standings that can't be resolved --/
-	if(ALLOW['EVENT_SCOREKEEP'] == TRUE){
+// // - Add a warning if there are ties in the standings that can't be resolved --/
+// 	if(ALLOW['EVENT_SCOREKEEP'] == TRUE){
 
-		$tournamentID = $_SESSION['tournamentID'];
-		$tiedFighters = findTiedFighters($tournamentID);
-		$poolsActive = isInProgress($tournamentID,'pool');
-		$bracketPopulated = isBracketPopulated($tournamentID);
+// 		$tournamentID = $_SESSION['tournamentID'];
+// 		$tiedFighters = findTiedFighters($tournamentID);
+// 		$poolsActive = isInProgress($tournamentID,'pool');
+// 		$bracketPopulated = isBracketPopulated($tournamentID);
 
-		if($tiedFighters != [] && $poolsActive == false && $bracketPopulated == false){
+// 		if($tiedFighters != [] && $poolsActive == false && $bracketPopulated == false){
 
-			$a['message'] = 'There are ties between the following fighters:<ul>';
-			foreach($tiedFighters as $fighter){
-				$a['message'] .= "<li>{$fighter}</li>";
-			}
-			$a['message'] .= "</ul>Scorecard has used all specified tiebreakers
-				and can not resolve the results. Place has been assigned randomly,
-				please take any necessary measures to break the tie if you intend
-				to seed a bracket based on this.";
+// 			$a['message'] = 'There are ties between the following fighters:<ul>';
+// 			foreach($tiedFighters as $fighter){
+// 				$a['message'] .= "<li>{$fighter}</li>";
+// 			}
+// 			$a['message'] .= "</ul>Scorecard has used all specified tiebreakers
+// 				and can not resolve the results. Place has been assigned randomly,
+// 				please take any necessary measures to break the tie if you intend
+// 				to seed a bracket based on this.";
 
-			$a['announcementID'] = -($tournamentID);
+// 			$a['announcementID'] = -($tournamentID);
 
-			$a['displayUntil'] = $currentTime + 99999;
-			$a['visibility'] = 'staff';
+// 			$a['displayUntil'] = $currentTime + 99999;
+// 			$a['visibility'] = 'staff';
 
-			$announcements[] = $a;
+// 			$announcements[] = $a;
 
-		}
-	}
+// 		}
+// 	}
 
 
 // -- Display announcements ---------------------------------------------------/
